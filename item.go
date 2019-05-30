1011e0aef783c2d64e99de225b5ae2114a9f92d4
joe miller <joeym@joeym.net>
2019-05-30 07:57:47 -0700

    add accessors for remaining org.freedesktop.Secret.Item properties
    https://specifications.freedesktop.org/secret-service/re03.html
    
    Adds methods to item: Created(), Modified(), Attributes()

--- item.go
+++ item.go
@@ -1,77 +1,110 @@
 package libsecret
 
-import "github.com/godbus/dbus"
+import (
+	"time"
 
+	"github.com/godbus/dbus"
+)
 
 type Item struct {
-  conn *dbus.Conn
-  dbus  dbus.BusObject
+	conn *dbus.Conn
+	dbus dbus.BusObject
 }
-
 
 func NewItem(conn *dbus.Conn, path dbus.ObjectPath) *Item {
-  return &Item{
-    conn: conn,
-    dbus: conn.Object(DBusServiceName, path),
-  }
+	return &Item{
+		conn: conn,
+		dbus: conn.Object(DBusServiceName, path),
+	}
 }
-
 
 func (item Item) Path() dbus.ObjectPath {
-  return item.dbus.Path()
+	return item.dbus.Path()
 }
-
 
 // READWRITE String Label;
 func (item *Item) Label() (string, error) {
-  val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Label")
-  if err != nil {
-    return "", err
-  }
+	val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Label")
+	if err != nil {
+		return "", err
+	}
 
-  return val.Value().(string), nil
+	return val.Value().(string), nil
 }
-
 
 // READ Boolean Locked;
 func (item *Item) Locked() (bool, error) {
-  val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Locked")
-  if err != nil {
-    return true, err
-  }
+	val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Locked")
+	if err != nil {
+		return true, err
+	}
 
-  return val.Value().(bool), nil
+	return val.Value().(bool), nil
 }
 
+// READ Uint64 Created;
+func (item *Item) Created() (time.Time, error) {
+	val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Created")
+	if err != nil {
+		return time.Time{}, err
+	}
+	v := val.Value().(uint64)
+	tm := time.Unix(int64(v), 0)
+
+	return tm, nil
+}
+
+// READ Uint64 Modified;
+func (item *Item) Modified() (time.Time, error) {
+	val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Modified")
+	if err != nil {
+		return time.Time{}, err
+	}
+	v := val.Value().(uint64)
+	tm := time.Unix(int64(v), 0)
+
+	return tm, nil
+}
+
+// READWRITE Dict<String,String> Attributes;
+func (item *Item) Attributes() (map[string]string, error) {
+	attributes := make(map[string]string)
+	val, err := item.dbus.GetProperty("org.freedesktop.Secret.Item.Attributes")
+	if err != nil {
+		return attributes, err
+	}
+	attributes = val.Value().(map[string]string)
+
+	return attributes, nil
+}
 
 // GetSecret (IN ObjectPath session, OUT Secret secret);
 func (item *Item) GetSecret(session *Session) (*Secret, error) {
-  secret := Secret{}
+	secret := Secret{}
 
-  err := item.dbus.Call("org.freedesktop.Secret.Item.GetSecret", 0, session.Path()).Store(&secret)
-  if err != nil {
-    return &Secret{}, err
-  }
+	err := item.dbus.Call("org.freedesktop.Secret.Item.GetSecret", 0, session.Path()).Store(&secret)
+	if err != nil {
+		return &Secret{}, err
+	}
 
-  return &secret, nil
+	return &secret, nil
 }
-
 
 // Delete (OUT ObjectPath Prompt);
 func (item *Item) Delete() error {
-  var prompt dbus.ObjectPath
+	var prompt dbus.ObjectPath
 
-  err := item.dbus.Call("org.freedesktop.Secret.Item.Delete", 0).Store(&prompt)
-  if err != nil {
-    return err
-  }
+	err := item.dbus.Call("org.freedesktop.Secret.Item.Delete", 0).Store(&prompt)
+	if err != nil {
+		return err
+	}
 
-  if isPrompt(prompt) {
-    prompt := NewPrompt(item.conn, prompt)
-    if _, err := prompt.Prompt(); err != nil {
-      return err
-    }
-  }
+	if isPrompt(prompt) {
+		prompt := NewPrompt(item.conn, prompt)
+		if _, err := prompt.Prompt(); err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
